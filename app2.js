3df8470622f2be977617fef06ede874d1cc87c4f
israaothman <israaayoub@hotmail.com>
2024-03-26 12:59:19 +0300

    hoisting and scope

---
+++ app2.js
@@ -0,0 +1,41 @@
+// Hoisting 
+
+// var username;
+// console.log(username);
+// let username = "Esraa";
+
+// console.log(age);
+
+// test();
+// test2();
+
+
+// function test(){
+//     console.log("Hello world")
+//     return 50;
+// }
+
+// const test3 = () => 50;
+
+// const test2 = () =>{
+//     console.log("test 2 ");
+// }
+
+
+
+// scope 
+// global scope 
+// local scope 
+
+// let x = 5;
+
+// function pluseOne(){
+//     let x = 3;
+//     x = x +1;
+//     console.log("inside", x)
+// }
+
+
+// pluseOne();
+
+// console.log("outside", x);
