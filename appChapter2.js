ef651f1336222247407ddc686d65f771049de29d
israaothman <israaayoub@hotmail.com>
2024-03-26 14:25:21 +0300

    loops and conditions

---
+++ appChapter2.js
@@ -0,0 +1,168 @@
+// conditions: 
+
+// mathmatical operations 
+// +
+// -
+// * 
+// / 
+
+
+// x = 5 assignment operation  
+// x + y == 5 Compression 
+// 5 === "5"   Compression with datatype console.log(5 == "5");
+// console.log(5 == "5");
+// true 
+// console.log(5 === "5");
+// false
+
+
+// logical operatores 
+// and && 
+// or || 
+// not ! 
+
+
+// <
+// >
+// <=
+// >=
+
+// let x = 5;
+// let y = 5;
+
+// if (x > y) {
+//     console.log("x is bigger than y ")
+// }
+
+// if(x>y){
+//     console.log("x is bigger than y ")
+// }else{
+//     console.log("y is bigger than x")
+// }
+
+// if (x > y) {
+//     console.log("x is bigger than y ")
+// } else if (x < y) {
+//     console.log("y is bigger than x")
+// } else {
+//     console.log("y is equal to x")
+// }
+
+
+// let username = "Ahmad";
+// let age = 28;
+
+// if (username == "Ahmad" && age == 25) {
+//     alert("Welcome Ahmad")
+// } else if (username == "Esraa" || age == 20) {
+//     alert("Welcoeme Esraa ")
+// } else {
+//     alert("Welcome")
+// }
+
+
+// 90-100 A 
+// 80-89 B
+// 70-79 C 
+// 69 and below F
+
+// Switch 
+
+// let mark = 88;
+// let result;
+
+// switch (true) {
+//     case (mark <= 100 && mark >= 90):
+//         result = "A"
+//         break;
+//     case (mark < 90 && mark >= 80):
+//         result = "B"
+//         break;
+//     case (mark < 80 && mark >= 70):
+//         result = "C"
+//         break;
+//     default:
+//         result = "F";
+// }
+
+// switch(mark){
+//     case 88 :
+//         result = "B";
+//         break;
+// }
+
+
+// let z = "0";
+
+// switch (z) {
+//   case 0:
+//     text = "Off";
+//     break;
+//   case 1:
+//     text = "On";
+//     break;
+//   default:
+//     text = "No value found";
+// }
+// console.log(result);
+
+
+
+// console.log("welcome")
+// console.log("welcome")
+// console.log("welcome")
+// console.log("welcome")
+// console.log("welcome")
+
+
+// for (let i = 0; i < 5; i++) {
+
+//     console.log(i);
+
+// }
+
+
+// let user = prompt("Enter your name ");
+
+// while(user != "esraa"){
+//     user = prompt("Enter your name ");
+// }
+
+// let j = 0;
+// while (j < 5) {
+//     console.log("welcome j");
+//     j++;
+// }
+
+// let user;
+
+// do {
+//     user = prompt("Enter your name ");
+// } while (user != "esraa")
+
+
+
+// for (let i = 0; i < 5; i++) {
+//     if(i == 3 ) continue;
+//    console.log(i);
+// }
+
+
+// for (let i = 0; i < 5; i++) {
+//     if(i == 3 ) break;
+//    console.log(i);
+// }
+
+
+
+
+// Task : 
+// - Create javaScript file called `app.js` and link it with `index.html`.
+//    - When the home page is running you will do the following requirements in `app.js`:
+//       - Ask the user to enter his/her name as input. keep asking until he/she enters a name 
+//       - Ask the user to enter his/her gender as input. The answer should be either (male or female).
+//       - Ask the user to enter his/her age as input and alert the user if the age is less than or equal to zero.
+//       - Alert a welcoming message with the name of the user 
+
+
+
