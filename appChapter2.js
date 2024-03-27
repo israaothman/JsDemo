2f7277bd4f7121bf54c52045c47aae5d1aa013da
israaothman <israaayoub@hotmail.com>
2024-03-27 13:32:23 +0300

    arrays and object

--- appChapter2.js
+++ appChapter2.js
@@ -155,6 +155,78 @@
 
 
 
+let username = "Esraa";
+// let age = 10;
+// let location = "Amman";
+
+//Arrays 
+ let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu","Fri"];
+                //  0      1       2     3      4    5 
+
+console.log(weekDays[0]);
+console.log(weekDays[3]);
+console.log(weekDays.length);
+console.log(weekDays[weekDays.length-1]);
+weekDays[6]= "Sat";
+weekDays.push("test") // add value to the end of the array
+weekDays.pop();  // remove value from the end of the array
+// shift 
+// unshift
+console.log(weekDays);
+
+
+
+for(let i=0; i <= weekDays.length-1 ; i++){
+    console.log("day : " + i + weekDays[i]);
+} 
+
+
+weekDays.forEach((val,idx)=>console.log(val));
+
+
+// map filter reduce 
+// time complicty 
+// clean code 
+
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+// let arr2 =[];
+// for(let i=0; i < arr.length ; i++) {
+//     if(arr[i]%2 == 0) {
+//         arr2.push(arr[i]);
+//     } 
+// }
+// console.log(arr2);
+
+// let arr3 = arr.filter((val)=>{
+//     if(val%2 == 0){
+//        return val;
+//     }   
+// })
+// console.log(arr3);
+
+// let arr4= arr.map((val)=>{
+//     if(val%2 == 0){
+//        return val;
+//     }   else{
+//         return "odd"
+//     }
+// })
+// console.log(arr4);
+
+
+
+
+
+//Objects 
+// let obj = {key:value}
+let user = {username:"ahmad",age:10,location:"Amman"}
+console.log(user.username);
+user.age = 20;
+console.log(user.age);
+user.gender="Male";
+console.log(user);
+
+
 
 // Task : 
 // - Create javaScript file called `app.js` and link it with `index.html`.
