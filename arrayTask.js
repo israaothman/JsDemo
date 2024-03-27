96605b8f41c76925508491fcc4e240861c669d1f
israaothman <israaayoub@hotmail.com>
2024-03-27 21:02:26 +0300

    task3

---
+++ arrayTask.js
@@ -0,0 +1,21 @@
+// Questions 
+
+
+// Q1: Write a function to find the minimum and maximum elements in an array. 
+// EX: 
+// input : [1,2,3,8,5,6,9,7,5,6] 
+// output : min = 1 / max = 9 
+
+
+
+// Q2: Write a function that takes an array an return a the reverse of it 
+// EX: 
+//  input: [1,2,3,4,5,6]
+//  output : [6,5,4,3,2,1,]
+
+
+
+// Q3: Write a function that takes an array an return true if it has string elements on it and false if not 
+// EX :
+// input : [1,2,"3",4]
+// output: true;
